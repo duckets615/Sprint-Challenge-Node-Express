a5daefb775e5d4f753bdb987357a7444fe2d8908
duckets615 <33557104+duckets615@users.noreply.github.com>
2019-03-22 16:26:58 -0400

    project list component

--- stretch/src/App.js
+++ stretch/src/App.js
@@ -1,28 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ProjectsList from './components/projectsList.js';
+import ProjectPage from './components/projectPage.js';
+import {withRouter,Route} from 'react-router-dom';
+
 
 class App extends Component {
+  componentDidMount() {
+    this.props.history.push('/projects');
+  }
   render() {
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Route exact path='/projects' component={ ProjectsList } />
+        <Route exact path='/projects/:id' component={ ProjectPage } />
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
--- stretch/src/components/project.js
+++ stretch/src/components/project.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+
 const Project=props=><div className='card'><Link to={`/projects/${props.data.id}`}><p>{props.data.name}</p></Link></div>
 
 export default Project;
