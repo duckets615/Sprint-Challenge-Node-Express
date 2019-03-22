6e46a5ba045c8cc31e91500c845a3402de62ae65
duckets615 <33557104+duckets615@users.noreply.github.com>
2019-03-22 13:40:55 -0400

    fixed post, added put request

--- index.js
+++ index.js
@@ -4,40 +4,95 @@ const cors = require('cors');
 const helmet = require('helmet');
 server.use(express.json(), cors(), helmet());
 
-//
-const projectDB = require('./data/helpers/projectModel');
-const actionDB = require('./data/helpers/actionModel');
+const projectsDB = require('./data/helpers/projectModel');
+const actionsDB = require('./data/helpers/actionModel');
 
 
 server.get('/api/projects', (req, res) => {
-  projectDB.get().then( projects => res.status(200).send(projects))
-  .catch(err => res.status(500).json({ messege: `Cannot retrieve project list` }));
+ projectsDB.get().then((projects) => res.status(200).json(projects))
+  .catch(() => {res.status(500).json({ error: "Could retrieve projects" })
+  });
 });
 
 server.get('/api/projects/:id', (req, res) => {
-  const { id } = req.params;
-  projectDB.get(id).then(project => {
-    !project ? res.status(404).json({ message: `No project with that id found` }) : res.status(200).json(project)
-  .catch(() => res.status(500).json({ messege: `Could not retrieve project`}))
+ const { id } = req.params
+ projectsDB.get(id).then(project => res.json(project))
+  .catch(() => {res.status(500).json({ error: "Could not find Project with that ID" })
   });
 });
 
-server.delete('/api/users/:id', (req, res) => {
+server.delete('/api/projects/:id', (req, res) => {
   const { id } = req.params;
-  projectDB.remove(id).then(project => {
-    res.status(200).json({ messege: `Project deleted `})})
-    .catch(() => res.status(500).json({ messege: `Project could not be deleted` }))
-  });
+  projectsDB.remove(id).then(() => res.json({message: "Project deleted from database."))
+  .catch(() => res.status(500).json({error: "Error deleting project from database."}))
+});
 
 server.post('/api/projects', (req, res) => {
-  const { name, description } = req.body;
-  if (!name || !description) res.status(404).json({ messege: `must provide title & description` }); return;
-  projectDB.insert(title, description).then(() => res.status(200).json({ messege: `project created`}))
-  .catch(() => res.status(500).json({ messege: `could not create user` }))
+ const { name, description, completed } = req.body;
+  if (name, description, completed) {
+    projectsDB.insert({ name, description, completed })
+    .then(({name, description, completed}) => res.status(200).json({name, description, completed}))
+  } else {
+   res.status(500).json({error: "Error adding project to database."})
+ };
+});
+
+
+server.put('/api/projects/:id', (req, res) => {
+ const { id } = req.params;
+ const {name, description, completed} = req.body;
+ if (name, description, completed && id) {
+  projectsDB.update(id, req.body).then(() => res.json({ message: "Project info updated" }))
+ } else {
+  res.catch(() => res.status(500).json({ error: "Error updating projects." }))
+ }
 });
 
 
 
 
 const port = 4000;
- server.listen(port, () => console.log(`server rolling on port ${port}`));
+server.listen(port, () => console.log(`server rolling on port ${port}`));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//
+// // server.post('/api/projects', (req, res) => {
+// //   const { name, description } = req.body;
+// //   if (!name || !description) return res.status(404).json({ messege: `must provide title & description` });
+// //   projectDB.insert(name, description).then(() => res.status(200).json({ messege: `project created` }))
+// //   .catch(() => res.status(500).json({ messege: `could not create user` }))
+// // });
+//
+//
+//
+//
